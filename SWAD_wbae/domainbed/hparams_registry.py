9b303422a8e8af86ab4df0b81a6cbcc95e2fd930
boyanglyu <40790897+boyanglyu@users.noreply.github.com>
2023-05-28 22:23:37 -0400

    Update hparams_registry.py

--- SWAD_wbae/domainbed/hparams_registry.py
+++ SWAD_wbae/domainbed/hparams_registry.py
@@ -40,43 +40,16 @@ def _hparams(algorithm, dataset, random_state):
     else:
         hparams["weight_decay"] = (1e-6, random_state.choice([1e-6,1e-4])) # Here
 
-    # if algorithm=="DNA":
-    #     hparams["bottleneck_dim"] = (1024, random_state.choice([1024,2048]))
-    #     hparams["dropout_rate"] = (0.5, random_state.choice([0.5,0.1]))
-    #     hparams["dropout_type"] = ('Bernoulli', 'Bernoulli')
-    #     hparams["lambda_v"] = (0.1, random_state.choice([0.01, 0.1, 1.0]))
-    # algorithm == "WBAE":
-        # 0725 only change the batch size 2^5 - 2^6.5 and use the following hyperpaermeters
-        # Use less steps is better
-        # _hparam('wbae_alpha', 1e-3, lambda r: r.choice([1e-3, 1e-2, 1e-1])) #0725
-        # _hparam('wbae_beta', 1e-3, lambda r: r.choice([1e-4, 1e-3, 1e-2, 1e-1])) #0725
-        # _hparam('wbae_blur', 10., lambda r: r.choice([0.5, 10., 50.])) #0725
-        
-        # 0727 will fix all the hyper-parameters, only tune the following hyper-parameters
-        # _hparam('wbae_alpha', 1e-3, lambda r: 10**r.uniform(-3, 0)) #0727
-        # _hparam('wbae_beta', 1e-3, lambda r: 10**r.uniform(-3, 0)) #0727
-        # _hparam('wbae_blur', 10., lambda r: r.choice([10., 30., 50])) #0727
-        
-        # 0729 will fix all the hyper-parameters, use batch size = 32 and change batch size to 64, only tune the following hyper-parameters
-        # 0810 alpha:-3.5, -2, beta -3.5, -1.5
+    
     if algorithm == "WBAE":
         hparams['wbae_alpha'] = (10**(-3), random_state.choice([10**(-3.5), 10**(-3), 10**(-2.5), 10**(-2)]))  
         hparams['wbae_beta'] = (10**(-2), random_state.choice([10**(-3.5), 10**(-3), 10**(-2), 10**(-1.5)]))
         
-    # if algorithm=="Independent_Inv":
-    #     hparams["alpha"] = (1, random_state.choice([0.1, 1.0, 10]))
-    #     hparams["beta"] = (1, random_state.choice([0.01, 0.1, 1.0]))
-    
+ 
 
     return hparams
 
-    # To reproduce the results of DNA, we recommend the hyperparameters searched on the validation set:
-    # PACS: (5e-5, 0.5, 1024, 0.1)
-    # VLCS: (3e-5, 0.5, 1024, 0.1)
-    # OfficeHome: (5e-5, 0.5, 1024, 0.1)
-    # TerraIncognita: (5e-5, 0.5, 1024, 0.1)
-    # DomainNet: (4e-5, 0.1, 2048, 0.1)
-    # Each tuple represents (lr, dropout_rate, bottleneck_dim, lambda_v).
+
 
 def default_hparams(algorithm, dataset):
     dummy_random_state = np.random.RandomState(0)

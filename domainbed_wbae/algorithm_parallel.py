cdf02ddaf87a64e2b61814f0efb31c34d4016435
boyanglyu <40790897+boyanglyu@users.noreply.github.com>
2024-02-25 14:44:41 -0500

    Create algorithm_parallel.py

---
+++ domainbed_wbae/algorithm_parallel.py
@@ -0,0 +1,87 @@
+# Adapt from DomainBed code
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.autograd as autograd
+from torch.autograd import Variable
+
+import copy
+import numpy as np
+from collections import defaultdict, OrderedDict
+from domainbed import networks_parallel
+
+
+
+ALGORITHMS = [
+    'ERM',
+]
+
+def get_algorithm_class(algorithm_name):
+    """Return the algorithm class with the given name."""
+    if algorithm_name not in globals():
+        raise NotImplementedError("Algorithm not found: {}".format(algorithm_name))
+    return globals()[algorithm_name]
+
+class Algorithm(torch.nn.Module):
+    """
+    A subclass of Algorithm implements a domain generalization algorithm.
+    Subclasses should implement the following:
+    - update()
+    - predict()
+    """
+    def __init__(self, input_shape, num_classes, num_domains, hparams):
+        super(Algorithm, self).__init__()
+        self.hparams = hparams
+
+    def update(self, minibatches, unlabeled=None):
+        """
+        Perform one update step, given a list of (x, y) tuples for all
+        environments.
+
+        Admits an optional list of unlabeled minibatches from the test domains,
+        when task is domain_adaptation.
+        """
+        raise NotImplementedError
+
+    def predict(self, x):
+        raise NotImplementedError
+
+class ERM(Algorithm):
+    """
+    Empirical Risk Minimization (ERM) with Pipeline parallel
+    """
+
+    def __init__(self, input_shape, num_classes, num_domains, hparams):
+        super(ERM, self).__init__(input_shape, num_classes, num_domains,
+                                  hparams)
+        self.featurizer = networks_parallel.Featurizer(input_shape, self.hparams)
+        self.classifier = networks_parallel.Classifier(
+            2048,
+            num_classes,
+            self.hparams['nonlinear_classifier'])
+
+        self.optimizer = torch.optim.Adam(
+            list(self.featurizer.parameters()) +\
+            list(self.classifier.parameters()),
+            lr=self.hparams["lr"],
+            weight_decay=self.hparams['weight_decay']
+        )
+
+
+    def update(self, minibatches, unlabeled=None):
+        all_x = torch.cat([x for x,y in minibatches])
+        all_y = torch.cat([y for x,y in minibatches])
+        loss = F.cross_entropy(self.predict(all_x), all_y)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        return {'loss': loss.item()}
+
+    def predict(self, x):
+        out = self.featurizer(x).local_value().squeeze()
+        return self.classifier(out)
+
+
